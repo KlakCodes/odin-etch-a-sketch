335c5882517b5be4fd6e1ff6f49d35c716ad74f3
KlakCodes <harrisonjclark@hotmail.com>
2024-05-30 16:40:35 +0100

    Created a function to successfully generate a grid with a specified amount of cells

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector(".grid");
 
-    for (let i = 0; i <= 256; i++) {
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.toggle("gridSquare");
-        grid.appendChild(gridSquare);
+    function createGrid(gridColumns) {
+        const gridCells = gridColumns * gridColumns;
+        // const cellWidth = (grid.clientWidth / gridColumns) - gridColumns;
+        const cellWidth = grid.clientWidth / gridColumns - 2;
+
+        for (let i = 0; i < gridCells; i++) {
+            const cell = document.createElement("div");
+            cell.classList.toggle("cell");
+            cell.style.border = "1px dashed red";
+            cell.style.flex = `1 1 ${cellWidth}px`;
+            cell.textContent = i + 1;
+            grid.appendChild(cell);
+        }
+
+        // Debug section START
+
+        console.log(`Total cells: ${gridCells}`);
+        console.log(`Grid W + H: ${gridColumns}`);
+        console.log(`Cell width: ${cellWidth}`);
+
+        console.log(`W: ${document.querySelector(".cell").clientWidth} x H: ${document.querySelector(".cell").clientHeight}`);
+
+        // Debug section END
     }
+
+    createGrid(16);
 })
