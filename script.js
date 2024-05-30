01d717340ff5087bdb222c205c3d7fea1f3652e2
KlakCodes <harrisonjclark@hotmail.com>
2024-05-30 19:09:08 +0100

    Implemented a random color when entering each square

--- script.js
+++ script.js
@@ -15,22 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
             grid.appendChild(cell);
         }
 
-        // Debug section START
-
-        console.log(`Total cells: ${gridCells}`);
-        console.log(`Grid W + H: ${gridColumns}`);
-        console.log(`Cell width: ${cellWidth}`);
-
-        console.log(`W: ${document.querySelector(".cell").clientWidth} x H: ${document.querySelector(".cell").clientHeight}`);
-
-        // Debug section END
-
         const cells = document.querySelectorAll(".cell");
 
         // Color the cell black when the mouse enters the cell
         cells.forEach((cell) => {
             cell.addEventListener("mouseenter", () => {
-                cell.style.backgroundColor = "black";
+
+                function randCol () {
+                    function randNum (max) {
+                      return Math.floor(Math.random() * max);
+                    }
+                  
+                    return `rgba(${randNum(256)}, ${randNum(256)}, ${randNum(256)}, 1)`
+                  }
+
+                cell.style.backgroundColor = randCol();
             });
         });
     }
@@ -50,13 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // When button is pressed, the user is prompted for the size of the new grid and the old one is cleared
     gridButton.addEventListener("click", () => {
-        // const userGridCount = prompt("How many columns and rows? (Min: 1 - Max: 100)");
-
-        // if(userGridCount && (userGridCount > 0 && userGridCount <= 100)) {
-        //     clearGrid();
-        //     createGrid(userGridCount);
-        // }
-
         const invalid = true;
 
         while (invalid) {
